73d78521ed3dfcd5dd5c0a8f0f188510c89830d4
pradeeprrjth <pradeepkumar82681@gmail.com>
2023-11-17 16:19:30 +0530

    Bug fixed

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // app.js
 const socket = io();
 
+// Handle socket connection events
 socket.on('connect', () => {
     console.log('Connected to server');
 });
@@ -11,44 +12,34 @@ socket.on('disconnect', () => {
 
 $(document).ready(() => {
     console.log("Document is ready");
+
+    const $messageForm = $('#message-form');
+    const $loadingIndicator = $('#loading-indicator');
     const $messageInput = $('#message-input');
-    const $sendButton = $('#send-button');
     const $chatMessages = $('#chat-messages');
     const $errorMessage = $('#error-messages');
 
-    $sendButton.click(() => {
+    $messageForm.submit((event) => {
+        event.preventDefault();
         const message = $messageInput.val();
         if (message) {
-            // Send the user's message to the server
+            $loadingIndicator.show();
             socket.emit('message', { message });
-    
-            // Send message to OpenAI API
-            $.post('/openai', { message }, (data) => {
-                if (data.error) {
-                    // Display the error message to the user
-                    $errorMessage.text(data.error);
-                } else {
-                    const reply = data.reply;
-    
-                    // Display the OpenAI response in the chat interface
-                    $chatMessages.append(`<div class="message bot">${reply}</div>`);
-    
-                    // Display the user's message in the chat interface
-                    $chatMessages.append(`<div class="message user">${message}</div>`);
-                }
-            });
-    
-            // Clear the input field
             $messageInput.val('');
         }
     });
 
     socket.on('message', (data) => {
-        // Display messages received from chat GPT
-        $chatMessages.append(`<div class="message">${data.message}</div>`);
+        $chatMessages.append(`<div class="message user">${data.message}</div>`);
+    });
+
+    socket.on('openai-response', (data) => {
+        $loadingIndicator.hide();
+        $chatMessages.append(`<div class="message bot">${data.reply}</div>`);
     });
 
     socket.on('error', (data) => {
+        $loadingIndicator.hide();
         $errorMessage.text(data.message);
     });
 });
--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// index.js
-
 require('dotenv').config();
 
 const express = require('express');
@@ -12,13 +10,14 @@ const path = require('path');
 
 app.use(express.static(__dirname));
 
-
 const PORT = process.env.PORT || 3000;
 
 const Message = mongoose.model('Message', {
     content: String,
+    userType: String,
     timestamp: { type: Date, default: Date.now },
 });
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
@@ -33,47 +32,47 @@ io.on('connection', (socket) => {
 
     socket.on('message', async (data) => {
         const { message } = data;
+        const userMessage = new Message({ content: message, userType: 'user' });
+        await userMessage.save();
 
-        // Save message to MongoDB
-        const newMessage = new Message({ content: message });
-        await newMessage.save();
-
-        // Broadcast message to all connected clients
         io.emit('message', { message });
+
+        // Add the user's message to the conversation history
+        conversationHistory.push({ role: 'user', content: message });
+
+        try {
+            const response = await openaiInstance.chat.completions.create({
+                model: 'gpt-3.5-turbo',
+                messages: conversationHistory,
+                max_tokens: 2048,
+                temperature: 1,
+            });
+
+            const aiResponse = response.choices[0].message.content;
+
+            const chatBotMessage = new Message({ content: aiResponse, userType: 'chatBot' });
+            await chatBotMessage.save();
+
+            conversationHistory.push({ role: 'assistant', content: aiResponse });
+
+            io.emit('openai-response', { reply: aiResponse.trim() });
+        } catch (error) {
+            console.error(error.message);
+            console.error(error);
+            io.emit('error', { message: error.message });
+        }
     });
 });
 
-const openaiApiKey = process.env.OPENAI_API_KEY
-
+const openaiApiKey = process.env.OPENAI_API_KEY;
 const openaiInstance = new openai.OpenAI({ key: openaiApiKey });
 
-app.use(express.json());
-
-app.post('/openai', async (req, res) => {
-    const { message } = req.body;
-
-    try {
-        const response = await openaiInstance.chat.completions.create({
-            model: 'text-davinci-003',
-            prompt: message,
-            max_tokens: 2048,
-            temperature: 1,
-        });
-
-        res.json({ reply: response.choices[0].text.trim() });
-
-    } catch (error) {
-        console.error(error.message);
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
-        io.emit('error', { message: error.message });
-    }
-});
 mongoose.connect(process.env.MONGODB_URI);
-
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 http.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+let conversationHistory = []; // Initialize an array to store conversation history
